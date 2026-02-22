1bd857bb44abd6be10c8d91c4d694a1db6b3791d
ElecTr1fy1 <danielmolbusiness@gmail.com>
2026-02-22 20:21:34 +0400

    V8.2: Fix Plan crash, Insights body, encoding, markdown, AI reorder

--- app/components/GlobalPanels.tsx
+++ app/components/GlobalPanels.tsx
@@ -1,8 +1,4 @@
 'use client';
-import { usePathname } from 'next/navigation';
-import SettingsDrawer from './SettingsDrawer';
-
 export default function GlobalPanels() {
-  const pathname = usePathname();
-  return (<><SettingsDrawer /></>);
+  return null;
 }
